6b8f778bc684fea2e3fbe613e7b237cc25c19924
kenoteb <kenoteb@onid.oregonstate.edu>
2018-02-12 16:45:08 -0800

    change LED color with post request

--- HelloServer/HelloServer.ino
+++ HelloServer/HelloServer.ino
@@ -3,12 +3,31 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#define PIN D6
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(30, PIN, NEO_GRB + NEO_KHZ800);
+
 #include "FS.h"
 #include "networkDetails.h"
 
 ESP8266WebServer server(80);
 
 const int led = 13;
+
+int red = 255, green = 0, blue = 0;
 
 void handleRoot() {
   digitalWrite(led, 1);
@@ -42,6 +61,16 @@ void handleNotFound(){
 }
 
 void setup(void){
+  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+  #if defined (__AVR_ATtiny85__)
+    if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+  #endif
+  // End of trinket special code
+
+
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+  
   pinMode(led, OUTPUT);
   digitalWrite(led, 0);
   Serial.begin(115200);
@@ -66,8 +95,11 @@ void setup(void){
 
   server.on("/", handleRoot);
 
-  server.on("/inline", [](){
-    server.send(200, "text/plain", "this works as well");
+  server.on("/set", [](){
+    red = server.arg("red").toInt();
+    green = server.arg("green").toInt();
+    blue = server.arg("blue").toInt();
+    server.send(200, "text/plain", "done");
   });
 
   server.onNotFound(handleNotFound);
@@ -77,5 +109,15 @@ void setup(void){
 }
 
 void loop(void){
+  colorWipe(strip.Color(red, green, blue), 50); // Red
   server.handleClient();
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
 }
